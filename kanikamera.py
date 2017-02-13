42a55c4adf033a57cc80c549f17ca872d5866262
Jaakko Moisio <jaakko@moisio.fi>
2017-02-13 21:48:51 +0200

    Add basic functionality
    The app now takes picture and uploads it to Dropbox, every 300 seconds. Only the
    app token is currently configurable.

--- kanikamera.py
+++ kanikamera.py
@@ -1,5 +1,27 @@
+from configparser import ConfigParser
+from datetime import datetime
+from io import BytesIO
+import os
+import time
+
+from dropbox import Dropbox
+from picamera import PiCamera
+
+def capture_and_upload(token):
+    imgfile = BytesIO()
+    with PiCamera(sensor_mode=3, resolution=(2592,1944)) as camera:
+        camera.capture(imgfile, format="jpeg", resize=(800,600))
+    upload_file = "/Kanikuvat/{}.jpg".format(datetime.now().strftime("%Y%m%d-%H%M%S"))
+    dropbox=Dropbox(token)
+    dropbox.files_upload(imgfile.getvalue(), upload_file)
+
 def main():
-    print("Hello, world!")
+    config = ConfigParser()
+    config.read(os.path.expanduser("~/.kanikamera"))
+    token = config.get("Dropbox", "Token")
+    while True:
+        capture_and_upload(token)
+        time.sleep(300)
 
 if __name__ == '__main__':
     main()
