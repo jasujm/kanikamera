8317de59a4d62d58206dee5d872447b3d2581151
Jaakko Moisio <jaakko@moisio.fi>
2017-02-13 20:19:02 +0200

    Initial commit
    This commit contains sample setup.py and code skeleton

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="Kanikamera",
+    py_modules="kanikamera",
+    entry_points={
+        "console_scripts" : [
+            "kanikamera = kanikamera:main"
+        ]
+    },
+    install_requires=[
+        "dropbox>=7.1", "picamera>=1.12"
+    ]
+)
